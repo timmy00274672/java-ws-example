f0b562f420a9f364d6c26c9a73f03652e3e1d140
timmy00274672 <timmy00274672@gmail.com>
2015-08-10 18:58:02 +0800

    modify client to be easy

--- client/src/main/java/com/diplab/HelloClient.java
+++ client/src/main/java/com/diplab/HelloClient.java
@@ -1,20 +1,13 @@
 package com.diplab;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
 
 public class HelloClient {
 
 	public static void main(String[] args) throws MalformedURLException {
-		URL url = new URL("http://localhost:9004/webservice/sayHello");
-		QName qname = new QName("http://diplab.com/", "HelloServiceImplService");
-		Service service = Service.create(url, qname);
+		HelloService helloServiceImplPort = new HelloServiceImplService().getHelloServiceImplPort();
 
-		HelloService hello = service.getPort(HelloService.class);
-		System.out.println(hello.sayHello("JavaHash.com"));
+		System.out.println(helloServiceImplPort.sayHello("JavaHash.com"));
 
 	}
 
